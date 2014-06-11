e05e541ae55f27c7484b57e7c2ac4e962f081902
Francois Ferrand <thetypz@gmail.com>
2014-06-11 09:38:54 +0200

    Fix crash.

--- src/interface/LocalListView.cpp
+++ src/interface/LocalListView.cpp
@@ -474,6 +474,7 @@ int CLocalListView::OnGetItemImage(long item) const
 	if (icon == -2)
 	{
 		wxString path = _T("");
+		bool isSymLink = false;
 		if (data->name != _T(".."))
 		{
 #ifdef __WXMSW__
@@ -482,10 +483,9 @@ int CLocalListView::OnGetItemImage(long item) const
 			else
 #endif
 				path = m_dir + data->name;
+			CLocalFileSystem::GetFileInfo(path, isSymLink, NULL, NULL, NULL);
 		}
 
-        bool isSymLink = false;
-        CLocalFileSystem::GetFileInfo(path, isSymLink, NULL, NULL, NULL);
 		icon = pThis->GetIconIndex(data->dir ? dir : file, path, true, isSymLink);
 	}
 	return icon;
